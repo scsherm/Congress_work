b184b8bfb5d1b594c80ee0761c8bd7017ab708e5
scsherm <scsherm@gmail.com>
2015-12-07 16:59:57 -0700

    grouping by bills and find max length bill

--- get_filepaths.py
+++ get_filepaths.py
@@ -14,14 +14,21 @@ def get_filepaths_votes():
 	return txt_file_list
 
 def get_filepaths():
-	txt_file_list = []
+	full_file_list = []
+
 	#scan subfolders for txt files
 	for root, dirs, files in os.walk("/Volumes/scsherm/congress/data"):
-		for file in files:
-			if file.endswith(".txt"):
-				print os.path.join(root, file)
-				txt_file_list.append(os.path.join(root, file))
-	return txt_file_list
+		if os.path.abspath(root).endswith(('/hconres', '/hjres', '/hr', '/hres', '/s', '/sconres', '/sjres', '/sres')):
+			for directory in os.listdir(os.path.abspath(root)):
+				d_file_list = []
+				if not directory.startswith('.'):
+					for r, d, fs in os.walk(os.path.abspath(root) + '/' + directory):
+						for file in fs:
+							if file.endswith(".txt"):
+								print os.path.join(r, file)
+								d_file_list.append(os.path.join(r, file))
+				full_file_list.append(d_file_list)
+	return full_file_list
 
 
 if __name__ == '__main__':
--- txt_to_df.py
+++ txt_to_df.py
@@ -1,33 +1,28 @@
 from get_filepaths import get_filepaths
 import pandas as pd 
 from pathlib import Path
+import numpy as np 
 
 def group_texts():
-	txt_file_list = get_filepaths()
+	full_file_list = get_filepaths()
 	txt_list = []
-	fl = [txt_file_list[0]]
-	for i in range(len(txt_file_list)):
-		print txt_file_list[i] #tracking
-		if i > 0:
-			if Path(txt_file_list[i]).parents[1] == Path(txt_file_list[i-1]).parents[1]:
-				with open(txt_file_list[i]) as f:
-					fl.append(f.read())
-			else:
-				print 'Next Section' #tracking
-				txt_list.append(fl)
-				with open(txt_file_list[i]) as f:
-					fl = [f.read()]
-	txt_list.append(fl)
+	for i in full_file_list:
+		if i:
+			print i #tracking
+			max_find = np.array([len(open(file).read()) for file in i])
+			max_find_num = np.where(max_find == max(max_find))[0][0]
+			with open(i[max_find_num]) as f:
+				txt_list.append(f.read())
 	return txt_list
 
 
 
 
-		with open(filename) as f:
-			print f
-			txt_list.append(f.read())
-	bill_txt_df = pd.DataFrame(txt_list)
-	return bill_txt_df
+	# 	with open(filename) as f:
+	# 		print f
+	# 		txt_list.append(f.read())
+	# bill_txt_df = pd.DataFrame(txt_list)
+	# return bill_txt_df
 
 if __name__ == '__main__':
 	bill_txt_df = to_df()
