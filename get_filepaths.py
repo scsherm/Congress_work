f0528c6362a4a780dc071eedd4b0954be17e92f0
scsherm <scsherm@gmail.com>
2015-12-07 13:48:23 -0700

    added function to get file paths for votes

--- get_filepaths.py
+++ get_filepaths.py
@@ -1,16 +1,27 @@
 import os
 
-def get_filepaths():
+def get_filepaths_votes():
 	txt_file_list = []
 	#scan subfolders for txt files
 	for root, dirs, files in os.walk("/Volumes/scsherm/congress/data"):
 		print os.path.abspath(root) 
+		#Check to see if in votes folder
 		if os.path.abspath(root).__contains__('/votes'):
-			print 'yes'
+			print 'yes' #verify
 			for file in files:
 				if file.endswith(".json"):
 					txt_file_list.append(os.path.join(root, file))
 	return txt_file_list
+
+def get_filepaths():
+	txt_file_list = []
+	#scan subfolders for txt files
+	for root, dirs, files in os.walk("/Volumes/scsherm/congress/data"):
+		for file in files:
+			if file.endswith(".txt"):
+				txt_file_list.append(os.path.join(root, file))
+	return txt_file_list
+
 
 if __name__ == '__main__':
 	txt_file_list = get_filepaths()
