8b3c9a1b93ed5f85f0a6e5f899b584ec3c20d46e
scsherm <scsherm@gmail.com>
2015-12-07 21:05:58 -0700

    dictionary with bill_id's

--- txt_to_df.py
+++ txt_to_df.py
@@ -5,15 +5,15 @@ import numpy as np
 
 def group_texts():
 	full_file_list = get_filepaths()
-	txt_list = []
+	txt_dict = {}
 	for i in full_file_list:
 		if i:
 			print i #tracking
 			max_find = np.array([len(open(file).read()) for file in i])
 			max_find_num = np.where(max_find == max(max_find))[0][0]
 			with open(i[max_find_num]) as f:
-				txt_list.append(f.read())
-	return txt_list
+				txt_dict[i[max_find_num].rsplit('/')[8] + '-' + i[max_find_num].rsplit('/')[5]] = [f.read()]
+	return txt_dict
 
 
 
@@ -25,4 +25,4 @@ def group_texts():
 	# return bill_txt_df
 
 if __name__ == '__main__':
-	txt_list = group_texts()
+	txt_dict = group_texts()
