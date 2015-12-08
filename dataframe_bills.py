73a331e4a649947f1dbac544cdd4b5a86485479a
scsherm <scsherm@gmail.com>
2015-12-08 16:06:20 -0700

    creates two dataframes

---
+++ dataframe_bills.py
@@ -0,0 +1,11 @@
+import pandas as pd 
+import numpy as np 
+from group_texts import group_texts
+
+def bills_to_df(txt_dict):
+	bills_df = pd.DataFrame.from_dict(txt_dict, orient = 'index')
+	return bills_df
+
+if __name__ == '__main__':
+	txt_dict = group_texts()
+	bills_df = bills_to_df(txt_dict)
---
+++ mongo_votes_to_pandas.py
@@ -0,0 +1,12 @@
+import pandas as pd 
+from pymongo import MongoClient
+
+def to_df():
+	client = MongoClient() #initiate mongo client
+	db = client.votes_database #connect to votes database
+	data = db.votes_table #connect ot votes table
+	votes_df = pd.DataFrame(list(data.find()))
+	return votes_df
+
+if __name__ == '__main__':
+	votes_df = to_df()
