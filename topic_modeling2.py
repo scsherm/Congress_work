54909d57ec14daaee36c7b64117fbd970f05b4e4
scsherm <scsherm@gmail.com>
2015-12-17 19:34:47 -0700

    plotting word cloud from nmf and lda

--- topic_modeling2.py
+++ topic_modeling2.py
@@ -12,48 +12,79 @@ from nltk.corpus import stopwords
 import Stemmer
 import nltk.stem 
 import pattern.en as en
+import numpy as np
 import unicodedata
 
 
 class StemmedTfidfVectorizer(TfidfVectorizer):
+    '''add lemmatization and ignore digits for TfidfVectorizer'''
     def build_analyzer(self):
         analyzer = super(TfidfVectorizer, self).build_analyzer()
         return lambda doc: (en.lemma(word) for word in analyzer(doc) if str.isdigit(unicodedata.normalize('NFKD', word).encode('ascii','ignore')) == False)
 
+
 class StemmedTfVectorizer(CountVectorizer):
+    '''add lemmatization and ignore digits for CountVectorizer'''
     def build_analyzer(self):
         analyzer = super(CountVectorizer, self).build_analyzer()
         return lambda doc: (en.lemma(word) for word in analyzer(doc) if str.isdigit(unicodedata.normalize('NFKD', word).encode('ascii','ignore')) == False)
 
+
 def print_top_words(model, feature_names, n_top_words):
-    topic_dict = {}
+    '''print topics and indicies and store them in dictionary'''
+    topic_dict = {} #empty dict
+    #enumerate lda/nmf components
     for topic_idx, topic in enumerate(model.components_):
         topic_dict[topic_idx] = " ".join([feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1]])
-        print("Topic #%d:" % topic_idx)
-        print(" ".join([feature_names[i]
-                        for i in topic.argsort()[:-n_top_words - 1:-1]]))
+        print("Topic {}:".format(topic_idx))
+        print(" ".join([feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1]]))
     print()
     return topic_dict
 
+
 def fit_nmf(tfidf):
+    '''takes in a tfidf sparse vector and finds the top topics'''
     nmf = NMF(n_components=n_topics, random_state=1, alpha=.1, l1_ratio=.5)
     nmf.fit(tfidf)
     tfidf_feature_names = tfidf_vectorizer.get_feature_names()
-    print_top_words(nmf, tfidf_feature_names, n_top_words)
-    return nmf
+    nmf_topic_dict = print_top_words(nmf, tfidf_feature_names, n_top_words)
+    return nmf, nmf_topic_dict
+
 
 def fit_lda(tf):
+    '''takes in a tf sparse vector and finds the top topics'''
     lda = LatentDirichletAllocation(n_topics=n_topics, max_iter=5, learning_method='online', learning_offset=50., random_state=0)
     lda.fit(tf)
-    return lda
+    tf_feature_names = tf_vectorizer.get_feature_names()
+    lda_topic_dict = print_top_words(lda, tf_feature_names, n_top_words)
+    return lda, lda_topic_dict
 
 
 if __name__ == '__main__':
     congressional_stop_words = open('congressional_stop_words.txt').read().split('\n')
-    stop_words = stopwords.words('english')
-    stop_words = stop_words + congressional_stop_words
-    n_topics = 10
+    stp_words = stopwords.words('english')
+    stp_words = stp_words + congressional_stop_words
+    n_topics = 5
     n_top_words = 20
+    bills_df_congress = pd.read_pickle('bills_df_congress')
+    bills_df_congress.drop(bills_df_congress.index[np.where(bills_df_congress.congress.isnull())[0][0]], inplace = True)
+    congress_topic_dict_nmf = {}
+    congress_topic_dict_lda = {}
+    nmf_words_weights = {}
+    lda_words_weights = {}
+    for congress in bills_df_congress.congress.unique():
+        print("running nmf/lda for congress {}".format(congress))
+        b = bills_df_congress.query('congress == @congress')
+        tfidf_vectorizer = StemmedTfidfVectorizer(max_features = 1000, stop_words = stp_words, max_df = 0.5)
+        tf_vectorizer = StemmedTfVectorizer(max_features = 1000, stop_words = stp_words, max_df = 0.5)
+        b_tfidf = tfidf_vectorizer.fit_transform(b[0])
+        b_tf = tf_vectorizer.fit_transform(b[0])
+        nmf, nmf_topic_dict = fit_nmf(b_tfidf)
+        lda, lda_topic_dict = fit_lda(b_tf)
+        congress_topic_dict_nmf[congress] = nmf_topic_dict
+        congress_topic_dict_lda[congress] = lda_topic_dict
+        nmf_words_weights[congress] = [zip(tfidf_vectorizer.get_feature_names(), i) for i in nmf.components_*1000]
+        lda_words_weights[congress] = [zip(tf_vectorizer.get_feature_names(), i) for i in lda.components_]
     with open('bills_tfidf_sparse.pkl', 'rb') as infile:
         tfidf = pickle.load(infile)
     with open('tfidf_vectorizer.pkl', 'rb') as infile:
@@ -62,16 +93,11 @@ if __name__ == '__main__':
         tf = pickle.load(infile)
     with open('tf_vectorizer.pkl', 'rb') as infile:
         tf_vectorizer = pickle.load(infile)
-    nmf = fit_nmf(tfidf)
-    lda = fit_lda(tf)
+    nmf, nmf_topic_dict = fit_nmf(tfidf)
+    lda, lda_topic_dict = fit_lda(tf)
     tfidf_feature_names = tfidf_vectorizer.get_feature_names()
-    nmf_topic_dict = print_top_words(nmf, tfidf_feature_names, n_top_words)
     tf_feature_names = tf_vectorizer.get_feature_names()
-    lda_topic_dict = print_top_words(lda, tf_feature_names, n_top_words)
 
-    bills_df = pd.read_pickle('bills_df')
-    t = StemmedTfidfVectorizer(max_features = 1000, stop_words = stop_words, max_df = 0.7)
-    bill_sparse = t.fit_transform(bills_df[0])
 
 
 
---
+++ word_clouds.py
@@ -0,0 +1,19 @@
+from wordcloud import WordCloud
+from itertools import repeat
+
+
+def plot_clouds(model_word_weights_dict):
+    for congress in model_word_weights_dict.keys():
+        for idx, tup_l in enumerate(model_word_weights_dict[congress]):
+            words = map(lambda x: list(repeat(x[0], int(x[1]))), tup_l)
+            words = [word for sublist in words for word in sublist]
+            text = ' '.join(words)
+            wc = WordCloud(background_color = 'white', width = 800, height = 1800).generate(text)
+            plt.figure(figsize = (9, 6))
+            plt.axis('off')
+            plt.set_title('Congress {} Main Topics {}'.format(congress, idx+1), y = 1.02, fontsize = 18)
+            plt.savefig('/Volumes/scsherm/Documents/Congress_work/plots/Congress_{}_{}'.format(congress, idx+1), format = 'png') 
+            plt.close()
+
+
+if __name__ == '__main__':
